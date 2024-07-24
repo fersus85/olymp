89e9baa51e8c29424c4f56ae5e50224dec710f98
deriabin_85 <deriabin_85@mail.ru>
2024-07-24 16:30:36 +0300

    Конфигурация логирования, добавил gitignore

---
+++ logging_config/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ("setup_logging",)
+
+from .log_config import setup_logging
---
+++ logging_config/log_config.py
@@ -0,0 +1,11 @@
+import json
+import pathlib
+import logging.config
+
+
+def setup_logging():
+    config_file = pathlib.Path('logging_config/log_config.json')
+
+    with open(config_file) as f_in:
+        config = json.load(f_in)
+        logging.config.dictConfig(config)
